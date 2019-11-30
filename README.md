a9f2a3364c82392145dec0befd96d9be5aaa441c
Alexita <alexahanthony@gmail.com>
2019-11-30 13:59:50 -0800

    uploading final version

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# password-generator
+# Password Generator
+
+# Table of Contents (Optional)
+N/A
+
+# Installation
+Click this URL to view website. https://alexahanthony.github.io/bootstrap-student-portfolio/
+
+# Usage
+This application generates a random password based on user-selected criteria. This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code.
+
+# Credits
+There were no collaborators, (other than classmates and teacher feedback).
+
+# Third-party assets: 
+http://placekitten.com for images
+https://www.toptal.com/designers/subtlepatterns/ for background image
+
+Tutorials: https://www.w3schools.com/
+
+# License
+N/A
+
+# Badges
+N/A
+
+# Contributing
+N/A
+
+# Tests
+N/A
---
+++ script.js
@@ -0,0 +1,112 @@
+
+//DON'T TOUCH THIS CODE! This code is adding click handlers and DOM manipulation to the page.  Edit the generatePassword function and all should work properly.
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var copyBtn = document.querySelector("#copy");
+
+
+////define global variables for password (lower, upper, ^%$, numbers)
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+var specialChar = ['!', '@', '#', '$', '%', '&', '*'];
+var sourceArray = [];
+
+//this function will fire when you click the generate password button on the page.  I've set it to alert "You've clicked a button" and return a password of password for now. Update it to make your password
+function generatePassword() {
+
+    //welcome the user
+    var confirmPassword = confirm("Would you like a randomly generated password?");
+    //present user with options 
+    if (confirmPassword) {
+        var confirmUpperCase = confirm("Should password contain uppercase letters?");
+        if (confirmUpperCase) {
+            sourceArray = sourceArray.concat(upperCase);
+            console.log(sourceArray);
+            console.log(confirmUpperCase);
+        }
+        var confirmLowerCase = confirm("Should password contain lowercase letters?");
+        if (confirmLowerCase) {
+            sourceArray = sourceArray.concat(lowerCase);
+            console.log(sourceArray);
+            console.log(confirmLowerCase);
+        }
+        var confirmNumbers = confirm("Should password contain numbers?");
+        if (confirmNumbers) {
+            sourceArray = sourceArray.concat(numbers);
+            console.log(sourceArray);
+            console.log(confirmNumbers);
+        }
+        var confirmSpecialChar = confirm("Should password contain special characters?");
+        if (confirmSpecialChar) {
+            sourceArray = sourceArray.concat(specialChar);
+            console.log(sourceArray);
+            console.log(confirmSpecialChar);
+        }
+         //if user doesnt select at lease one option, inform user they muse select one option
+        if (sourceArray.length < 1) {
+            alert("You must select at lease one option")
+            return "invalid"
+        }
+
+        //select password length... if user doesnt select an option within 8 - 128 characters, inform user they muse type 8 - 128 and bring them back through the selection process again
+        var numSelect = prompt("How many characters do you want your password to be? (must be between 8 - 128)");
+        var valid = false;
+        if (numSelect <= 8) {
+            alert("Value must be greater than 8. Please try again.");
+            return "invalid"
+
+        }
+        else if (numSelect >= 128) {
+            alert("Value must be less than 128 characters. Please try again.");
+            return "invalid"
+        }
+        else {
+            var passwordArray = []
+            //randomly select characters until it is as long as user asked it to be 
+            for (i = 0; i < numSelect; i++) {
+                passwordArray.push(sourceArray[Math.floor(Math.random() * sourceArray.length)]);
+                console.log(passwordArray);
+            }
+            // Remove commas from final password result using join method
+            return passwordArray.join("");
+        }
+    }
+    else {
+        alert("OK, no password for you!");
+        return "No Password Generated"
+    }
+    return "invalid"
+}
+
+// Write password to the #password input
+function writePassword() {
+    var password;
+    do { 
+        password = generatePassword();
+    }
+    while (password == "invalid")
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+
+    copyBtn.removeAttribute("disabled");
+    copyBtn.focus();
+}
+
+function copyToClipboard() {
+    var copyText = document.querySelector("#password");
+    copyText.select();
+    //copyText.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    alert("Copied the text: " + copyText.value)
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+copyBtn.addEventListener("click", copyToClipboard);
+
+
+// Your application should have a clean and polished user interface and be responsive, ensuring that it adapts to multiple screen sizes.
+// Your application should be deployed to GitHub Pages.
+// Your application's GitHub repository should contain a README.md file explaining the purpose and functionality of the application. The README.md file should include a screenshot of the completed application as well as a link to the deployed GitHub Pages URL.
